67b3bf654073ccdbe6c2c8d530e036a6e637f83c
Welser Muñoz <welser.m.r@gmail.com>
2019-08-21 00:41:30 -0500

    Added filter for tags and ingredients.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -279,5 +279,3 @@ class RecipeImageUploadTests(TestCase):
         self.assertIn(serializer1.data, res.data)
         self.assertIn(serializer2.data, res.data)
         self.assertNotIn(serializer3, res.data)
-
-
